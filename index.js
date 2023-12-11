10aebf1ed574da643cfed1a7d672f2a3bfbef6e9
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2023-12-11 11:46:41 -0500

    player and enemy sprite created

--- index.js
+++ index.js
@@ -5,3 +5,65 @@ canvas.width = 1024;
 canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+enemy.draw();
+
+console.log(player);
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+};
+
+animate();
