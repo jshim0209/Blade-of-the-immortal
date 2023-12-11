f2d8b84fe03c18f670bd98d222bce627b53c9e71
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2023-12-11 13:22:21 -0500

    refactor last key to character specific

--- index.js
+++ index.js
@@ -75,8 +75,6 @@ const keys = {
   },
 };
 
-let lastKey;
-
 const animate = () => {
   window.requestAnimationFrame(animate);
   canvasContext.fillStyle = "black";
@@ -88,9 +86,9 @@ const animate = () => {
   enemy.velocity.x = 0;
 
   // player movement
-  if (keys.a.pressed && lastKey === "a") {
+  if (keys.a.pressed && player.lastKey === "a") {
     player.velocity.x = -1;
-  } else if (keys.d.pressed && lastKey === "d") {
+  } else if (keys.d.pressed && player.lastKey === "d") {
     player.velocity.x = 1;
   }
 
@@ -109,11 +107,11 @@ window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "d":
       keys.d.pressed = true;
-      lastKey = "d";
+      player.lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
-      lastKey = "a";
+      player.lastKey = "a";
       break;
     case "w":
       player.velocity.y = -10;
